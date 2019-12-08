b2955e5bead85dd6d8b16e8dabadc160e80cf5dd
Silvan S. Batistella <silvan.batistella@gmail.com>
2019-12-08 07:02:57 -0300

    Criado Objeto Validation e Feita Validação para o UserDTO

---
+++ AppLog/Domain/Models/Util/Validation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppLog.Domain.Models.Util
+{
+    public class Validation
+    {
+
+        public string AttributeName { get; set; }
+        public string MessageError { get; set; }
+       public Validation(string attributeName, string message)
+        {
+            this.AttributeName = attributeName;
+            this.MessageError = message;
+        }
+    }
+}
--- AppLog/Dto/UserDto.cs
+++ AppLog/Dto/UserDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppLog.Domain.Models.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,5 +12,24 @@ namespace AppLog.Dto
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public IList<Validation> ValidateObj()
+        {
+            IList<Validation> validation = new List<Validation>();
+            if (this.Email.Equals(""))
+            {
+                validation.Add(new Validation("Email", "O campo E-Mail é Obrigatório"));
+            }
+            if (this.Name.Equals(""))
+            {
+                validation.Add(new Validation("Nome", "O campo Nome é Obrigatório"));
+            }
+            if (this.Password.Equals(""))
+            {
+                validation.Add(new Validation("Senha", "O campo Senha é Obrigatório"));
+            }
+
+            return validation;
+        }
     }
 }
